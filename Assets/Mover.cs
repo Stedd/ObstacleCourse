88d1e57b53d85729f7d397ab962651ff53ebb9cb
Stedd <tor.andre.paulsen@gmail.com>
2021-09-05 09:33:56 +0200

    Basic movement

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float speedFactor = 0.0001f;
+    [SerializeField] float speedFactor; 
+    [SerializeField] Vector3 speedVec; 
+    [SerializeField] Vector3 controlVec;
     Vector3 speed = new Vector3();
     // Start is called before the first frame update
     void Start()
     {
-
+        speedFactor = 2.5f;
+        speedVec    = updateSpeed(speedFactor);
+        controlVec  = new Vector3 (0,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        speed.x = speedFactor*Mathf.Sin(Time.realtimeSinceStartup);
-        speed.y = 0;
-        speed.z = speedFactor*Mathf.Cos(Time.realtimeSinceStartup);
-        // transform.Position(0,0,0);
-        transform.Translate(speed);
+        // speedFactor     = 0.0015f;
+        speedVec        = updateSpeed(speedFactor);
+
+        controlVec.x    = Input.GetAxis("Horizontal");
+        controlVec.z    = Input.GetAxis("Vertical");
+        controlVec.Normalize();
+        
+        speed = Vector3.Scale(controlVec, speedVec);
+
+        transform.Translate(speed*Time.deltaTime);
     }
+
+    Vector3 updateSpeed(float _factor){
+        return new Vector3(_factor, _factor, _factor);
+    }
+
+
 }
